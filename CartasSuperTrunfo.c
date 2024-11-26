634e423175e3863119ce585acfbb3b97d122acfa
Sillas04 <sillas.lima16@outlook.com>
2024-11-26 14:28:29 -0300

    Atualizar o CartasSuperTrunfo.c
    Exclui algumas linhas de códigos desnecessárias

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,20 +6,10 @@
 // Siga os comentários para implementar cada parte do desafio.
 int main() {
     // Declarando variaveis para armazenar estados
-    char estado_A[30];
-    char estado_B[30];
-    char estado_C[30];
-    char estado_D[30];
-    char estado_E[30];
-    char estado_F[30];
-    char estado_G[30];
-    char estado_H[30];
-
+    char estado_A[30], estado_B[30], estado_C[30], estado_D[30], estado_E[30], estado_F[30], estado_G[30], estado_H[30];
+    
     // Declarando variaveis para armazenar nome da cidade.
-    char cidade_01[30];
-    char cidade_02[30];
-    char cidade_03[30];
-    char cidade_04[30];
+    char cidade_01[30], cidade_02[30], cidade_03[30], cidade_04[30];
 
     // Declarando variaveis para cada atributo da cidade.    
     char cod_cidade01[3], cod_cidade02[3];
@@ -28,9 +18,9 @@ int main() {
     float pib_c1, pib_c2;       
     int pontos_turisticos_c1, pontos_turisticos_c2;
 
-    // Calculoss -----------------------------------
-    double densidade_pp01, densidade_pp02;   
-    double pib_capta01, pib_capta02;   
+    // Variáveis de densidade e PIBpcpt
+    float densidade_pp01, densidade_pp02;   
+    float pib_capta01, pib_capta02;   
 
     // atribuindo valor a variavel "estado_A".
     printf("Digite o nome do Estado:\n");
@@ -59,24 +49,11 @@ int main() {
     scanf("%d", &pontos_turisticos_c1);
 
     // Calculo da densidade populacional
-    //************ Aqui você pode usar o (float), porque ele consome menos memória que o (double)
-    densidade_pp01 = (double) habitantes_c1 / km_area_c1;
-
-    pib_capta01 = (double) (pib_c1 * 1e9) / habitantes_c1;         
+    densidade_pp01 = (float) habitantes_c1 / km_area_c1;
+    pib_capta01 = (float) (pib_c1 * 1e9) / habitantes_c1;         
 
    // Mensagem de sucesso ao cadastrar
     printf("\n****** Carta cadastrada com sucesso! ****** \n\n");
-    
-    //************* Eu colocaria a impressão dos dados na carta no final, depois de cadastrar as duas cidades.
-    // Exibição dos Dados das Cartas:
-    printf("Cidade: %s \n", cidade_01);
-    printf("Código: %s \n", cod_cidade01);
-    printf("População: %d \n", habitantes_c1);
-    printf("Área: %.1f Km²\n", km_area_c1);
-    printf("PIB: R$ %.1f \n", pib_c1);
-    printf("Pontos Turísticos: %d \n", pontos_turisticos_c1);
-    printf("Densidade Populacional: %.1f hab/Km²\n", densidade_pp01);
-    printf("PIB per Capta: R$ %.2f\n\n", pib_capta01);
 
      // ------------------------ Cadastro da segunda cidade do "estado A".
     printf("Digite o nome da 2° cidade do estado de: %s \n", estado_A);
@@ -97,25 +74,15 @@ int main() {
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &pontos_turisticos_c2);  
 
-    // Calculo da densidade populacional
-    densidade_pp02 = (double) habitantes_c2 / km_area_c2;
-    pib_capta02 = (double) (pib_c2 * 1e9) / habitantes_c2;    
+    // Calculo densidade e PIB
+    densidade_pp02 = (float) habitantes_c2 / km_area_c2;
+    pib_capta02 = (float) (pib_c2 * 1e9) / habitantes_c2;    
 
    // Mensagem de sucesso ao cadastrar
-    printf("\n****** Carta cadastrada com sucesso! ****** \n\n");
-
-    // Exibição dos Dados das Cartas:
-    printf("Cidade: %s \n", cidade_02);
-    printf("Código: %s \n", cod_cidade02);
-    printf("População: %d \n", habitantes_c2);
-    printf("Área: %1f Km²\n", km_area_c2);
-    printf("PIB: R$%.1f \n", pib_c2);
-    printf("Pontos Turísticos: %d \n", pontos_turisticos_c2);
-    printf("Densidade Populacional: %.1f\n", densidade_pp02);    
-    printf("PIB per Capta: R$ %.2f\n\n", pib_capta02);    
+    printf("\n****** Carta cadastrada com sucesso! ****** \n\n");  
 
     //----------------------- EXIBINDO OS DADOS DE CADA CIDADE DE FORMA ORGANIZADA!!!  
-    //**************** Poderia usar somente esse print abaixo, mas não está errado!!! Só um comentário.
+    
     //Cidade 01
     printf("****** Primeira cidade do estado de: %s ******\n\n", estado_A);
     printf("Cidade: %s \nCódigo: %s \nPopulação: %d \nÁrea: %.1f Km² \nPIB: R$ %.1f \nPontos turísticos: %d \nDensidade Populacional: %.2f\n PIB per Capta: R$ %.2f\n\n", cidade_01, cod_cidade01, habitantes_c1, km_area_c1,pib_c1, pontos_turisticos_c1, densidade_pp01, pib_capta01);

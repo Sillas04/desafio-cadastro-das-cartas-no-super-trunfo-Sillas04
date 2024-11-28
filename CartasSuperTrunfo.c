abddc623549d5373571a56717087e38c1f2471f1
Sillas04 <sillas.lima16@outlook.com>
2024-11-28 15:57:54 -0300

    Atualizar o CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,18 +9,18 @@ int main() {
     char estado_A[30], estado_B[30];
     
     // Declarando variaveis para armazenar nome da cidade.
-    char cidade_01[30], cidade_02[30];
+    char cidade_a01[30], cidade_a02[30];
 
     // Declarando variaveis para cada atributo da cidade.    
-    char cod_cidade01[4], cod_cidade02[4];
-    unsigned long int habitantes_c1, habitantes_c2;
-    float km_area_c1, km_area_c2;
-    float pib_c1, pib_c2;       
-    int pontos_turisticos_c1, pontos_turisticos_c2;
+    char cod_cidade_a01[4], cod_cidade_a02[4];
+    unsigned long int habitantes_a01, habitantes_a02;
+    float km_area_a01, km_area_a02;
+    float pib_a01, pib_a02;       
+    int pontos_turisticos_a01, pontos_turisticos_a02;
 
     // Variáveis de densidade e PIBpcpt
-    float densidade_pp01, densidade_pp02;   
-    float pib_capta01, pib_capta02;   
+    float densidade_pp_a01, densidade_pp_a02;   
+    float pib_capta_a01, pib_capta_a02;   
 
     // atribuindo valor a variavel "estado_A".
     printf("Digite o nome do Estado:\n");
@@ -31,52 +31,52 @@ int main() {
 
     // --------------------- Cadastro da primeira cidade do estado_A.
     printf("Digite o nome da 1° cidade do estado de: %s \n", estado_A);
-    scanf("%30s", cidade_01);
+    scanf("%30s", cidade_a01);
 
     printf("Digite o código da cidade: \n");
-    scanf("%4s", cod_cidade01);    
+    scanf("%4s", cod_cidade_a01);    
 
     printf("Digite a população da cidade:\n");
-    scanf("%lu", &habitantes_c1);
+    scanf("%lu", &habitantes_a01);
 
     printf("Digite a área em Km²: \n");
-    scanf("%f", &km_area_c1);
+    scanf("%f", &km_area_a01);
 
     printf("Digite o PIB da cidade: \n");
-    scanf("%f", &pib_c1);
+    scanf("%f", &pib_a01);
 
     printf("Digite o número de pontos turísticos: \n");
-    scanf("%d", &pontos_turisticos_c1);
+    scanf("%d", &pontos_turisticos_a01);
 
     // Calculo da densidade populacional
-    densidade_pp01 = (float) (habitantes_c1 * 1000000) / km_area_c1;
-    pib_capta01 = (float) (pib_c1 * 1000000000) / habitantes_c1;         
+    densidade_pp_a01 = (float) (habitantes_a01 * 1000000) / km_area_a01;
+    pib_capta01 = (float) (pib_a01 * 1000000000) / habitantes_a01;         
 
    // Mensagem de sucesso ao cadastrar
     printf("\n****** Carta cadastrada com sucesso! ****** \n\n");
 
      // ------------------------ Cadastro da segunda cidade do "estado A".
     printf("Digite o nome da 2° cidade do estado de: %s \n", estado_A);
-    scanf("%30s", cidade_02);
+    scanf("%30s", cidade_a02);
 
     printf("Digite o código da cidade: \n");
-    scanf("%4s", cod_cidade02);
+    scanf("%4s", cod_cidade_a02);
 
     printf("Digite a população da cidade:\n");
-    scanf("%lu", &habitantes_c2);
+    scanf("%lu", &habitantes_a02);
 
     printf("Digite a área em Km²: \n");
-    scanf("%f", &km_area_c2);
+    scanf("%f", &km_area_a02);
 
     printf("Digite o PIB da cidade: \n");
-    scanf("%f", &pib_c2);
+    scanf("%f", &pib_a02);
 
     printf("Digite o número de pontos turísticos: \n");
-    scanf("%d", &pontos_turisticos_c2);  
+    scanf("%d", &pontos_turisticos_a02);  
 
     // Calculo densidade e PIB
-    densidade_pp02 = (float) (habitantes_c2 * 1000000) / km_area_c2;
-    pib_capta02 = (float) (pib_c2 * 1000000000) / habitantes_c2;    
+    densidade_pp02 = (float) (habitantes_a02 * 1000000) / km_area_a02;
+    pib_capta02 = (float) (pib_a02 * 1000000000) / habitantes_a02;    
 
    // Mensagem de sucesso ao cadastrar
     printf("\n****** Carta cadastrada com sucesso! ****** \n\n");  
@@ -85,11 +85,11 @@ int main() {
     
     //Cidade 01
     printf("****** Primeira cidade do estado de: %s ******\n\n", estado_A);
-    printf("Cidade: %s \nCódigo: %s \nPopulação: %lu Habitantes \nÁrea: %.1f Km² \nPIB: R$ %.1f Bilhões \nPontos turísticos: %d \nDensidade Populacional: %.2f hab/Km² \nPIB per Capta: R$ %.2ld\n\n", cidade_01, cod_cidade01, habitantes_c1, km_area_c1,pib_c1, pontos_turisticos_c1, densidade_pp01, pib_capta01);
+    printf("Cidade: %s \nCódigo: %s \nPopulação: %lu Habitantes \nÁrea: %.1f Km² \nPIB: R$ %.1f Bilhões \nPontos turísticos: %d \nDensidade Populacional: %.2f hab/Km² \nPIB per Capta: R$ %.2f\n\n", cidade_a01, cod_cidade_a01, habitantes_a01, km_area_a01,pib_a01, pontos_turisticos_a01, densidade_pp_a01, pib_capta_a01);
 
     //Cidade 02
     printf("****** Segunda cidade do estado de: %s ****** \n\n", estado_A);
-    printf("Cidade: %s \nCódigo: %s \nPopulação: %lu Habitantes \nÁrea: %.1f Km² \nPIB: R$ %.1f Bilhões \nPontos turísticos: %d \nDensidade Populacional: %.2f  hab/Km² \nPIB per Capta: R$ %.2ld\n\n", cidade_02, cod_cidade02, habitantes_c2, km_area_c2,pib_c2, pontos_turisticos_c2, densidade_pp02, pib_capta02);
+    printf("Cidade: %s \nCódigo: %s \nPopulação: %lu Habitantes \nÁrea: %.1f Km² \nPIB: R$ %.1f Bilhões \nPontos turísticos: %d \nDensidade Populacional: %.2f  hab/Km² \nPIB per Capta: R$ %.2f\n\n", cidade_a02, cod_cidade_a02, habitantes_a02, km_area_a02,pib_a02, pontos_turisticos_a02, densidade_pp_a02, pib_capta_a02);
 
     return 0;
 }
